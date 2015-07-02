1a8bd5046536933a80fcaf0dc1e83c7a3b5f7f07
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-02 18:59:34 -0400

    added base view controller for individual article view controller

---
+++ PubSavvySwipe/Articles/PSArticleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSArticleViewController.h
+//  PubSavvySwipe
+//
+//  Created by Dan Kwon on 7/2/15.
+//  Copyright (c) 2015 FrameResearch. All rights reserved.
+//
+
+#import "PSViewController.h"
+
+@interface PSArticleViewController : PSViewController
+
+@end
---
+++ PubSavvySwipe/Articles/PSArticleViewController.m
@@ -0,0 +1,36 @@
+//
+//  PSArticleViewController.m
+//  PubSavvySwipe
+//
+//  Created by Dan Kwon on 7/2/15.
+//  Copyright (c) 2015 FrameResearch. All rights reserved.
+
+
+#import "PSArticleViewController.h"
+
+@implementation PSArticleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self){
+        
+    }
+    
+    return self;
+}
+
+
+- (void)loadView
+{
+    UIView *view = [self baseView];
+    view.backgroundColor = kLightBlue;
+//    CGRect frame = view.frame;
+
+    
+    self.view = view;
+}
+
+
+
+@end
