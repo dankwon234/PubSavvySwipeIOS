6836afcba5923e6cbb18648b6ce879efc6487cbd
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-02 18:28:24 -0400

    added search bar and connected delegate

--- PubSavvySwipe/Articles/PSSearchViewController.m
+++ PubSavvySwipe/Articles/PSSearchViewController.m
@@ -8,7 +8,7 @@
 
 #import "PSSearchViewController.h"
 
-@interface PSSearchViewController ()
+@interface PSSearchViewController() <UISearchBarDelegate>
 @property (strong, nonatomic) UISearchBar *searchBar;
 @end
 
@@ -18,6 +18,15 @@
 {
     UIView *view = [self baseView];
     view.backgroundColor = [UIColor redColor];
+    CGRect frame = view.frame;
+    
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0.0f, 0.0f, frame.size.width, 44.0f)];
+    self.searchBar.delegate = self;
+    [view addSubview:self.searchBar];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [view addGestureRecognizer:tap];
+    
     
     self.view = view;
 }
@@ -27,5 +36,26 @@
     [super viewDidLoad];
     [self addMenuButton];
 }
+
+- (void)dismissKeyboard
+{
+    [self.searchBar resignFirstResponder];
+}
+
+#pragma mark - SearchBarDelegate
+- (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar
+{
+    NSLog(@"searchBarShouldBeginEditing:");
+    return YES;
+}
+
+//- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar;                     // called when text starts editing
+//- (BOOL)searchBarShouldEndEditing:(UISearchBar *)searchBar;                        // return NO to not resign first responder
+//- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar;                       // called when text ends editing
+//- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText;   // called when text changes (including clear)
+//- (BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text NS_AVAILABLE_IOS(3_0); // called before text changes
+//
+//- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar;
+
 
 @end
