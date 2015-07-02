95ef96898bef614b48b87264bf7cc90fbc1db302
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-02 19:29:54 -0400

    base table cell for displaying articles in search table

---
+++ PubSavvySwipe/Articles/PSArticleCell.h
@@ -0,0 +1,13 @@
+//
+//  PSArticleCell.h
+//  PubSavvySwipe
+//
+//  Created by Dan Kwon on 7/2/15.
+//  Copyright (c) 2015 FrameResearch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSArticleCell : UITableViewCell
+
+@end
