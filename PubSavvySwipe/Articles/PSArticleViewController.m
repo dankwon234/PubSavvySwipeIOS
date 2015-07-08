48b007897b5f896abc4095818af17aba84feebba
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-08 03:05:24 -0400

    removed web view from article view controller

--- PubSavvySwipe/Articles/PSArticleViewController.m
+++ PubSavvySwipe/Articles/PSArticleViewController.m
@@ -9,7 +9,7 @@
 #import "PSArticleViewController.h"
 
 @interface PSArticleViewController() <UIWebViewDelegate>
-@property (strong, nonatomic) UIWebView *articleWebview;
+
 @end
 
 @implementation PSArticleViewController
@@ -31,12 +31,8 @@
 {
     UIView *view = [self baseView];
     view.backgroundColor = kLightBlue;
-    CGRect frame = view.frame;
+//    CGRect frame = view.frame;
     
-    self.articleWebview = [[UIWebView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height-20.0f)];
-    self.articleWebview.delegate = self;
-    self.articleWebview.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
-    [view addSubview:self.articleWebview];
     
     
     self.view = view;
@@ -47,32 +43,8 @@
     [super viewDidLoad];
     [self addCustomBackButton];
     
-    [self.loadingIndicator startLoading];
-    NSString *url = [NSString stringWithFormat:@"http://www.ncbi.nlm.nih.gov/m/pubmed/%@/", self.article.pmid];
-    [self.articleWebview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
 }
 
-#pragma mark - UIWebviewDelegate
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
-    NSLog(@"webView shouldStartLoadWithRequest: %@", request.URL.absoluteString);
-    return YES;
-}
-
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-    
-}
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    [self.loadingIndicator stopLoading];
-
-}
-
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-    
-}
 
 
 
