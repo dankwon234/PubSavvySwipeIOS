92a8624ef51e3b31e3912c41bb7f91a50d87dc47
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-08 02:31:57 -0400

    set up search view controller same as featured articles view controller

--- PubSavvySwipe/Articles/PSSearchViewController.m
+++ PubSavvySwipe/Articles/PSSearchViewController.m
@@ -9,21 +9,32 @@
 #import "PSSearchViewController.h"
 #import "PSArticleViewController.h"
 #import "PSArticle.h"
-#import "PSArticleCell.h"
+#import "PSArticleView.h"
+
+
 
 @interface PSSearchViewController() <UISearchBarDelegate>
 @property (strong, nonatomic) UISearchBar *searchBar;
 @property (strong, nonatomic) NSMutableArray *searchResults;
+@property (strong, nonatomic) PSArticle *currentArticle;
+@property (strong, nonatomic) PSArticleView *topView;
+@property (nonatomic) int index;
 @end
 
+#define kPadding 12.0f
+#define kSetSize 10
+
+
 @implementation PSSearchViewController
+@synthesize currentArticle = _currentArticle;
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self){
+        self.currentArticle = nil;
         self.searchResults = [NSMutableArray array];
-        
     }
     
     return self;
@@ -43,6 +54,35 @@
     self.searchBar.autocorrectionType = UITextAutocorrectionTypeYes;
     [view addSubview:self.searchBar];
     
+    CGFloat w = 0.5f*(frame.size.width-3*kPadding);
+    CGFloat y = frame.size.height-h-kPadding-20.0f;
+    
+    UIButton *btnDislike = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnDislike.frame = CGRectMake(kPadding, y, w, h);
+    btnDislike.backgroundColor = [UIColor lightGrayColor];
+    btnDislike.layer.borderColor = [[UIColor grayColor] CGColor];
+    btnDislike.layer.borderWidth = 0.5f;
+    btnDislike.layer.cornerRadius = 2.0f;
+    btnDislike.layer.masksToBounds = YES;
+    btnDislike.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    [btnDislike addTarget:self action:@selector(dislikeArticle) forControlEvents:UIControlEventTouchUpInside];
+    [btnDislike setTitle:@"SKIP" forState:UIControlStateNormal];
+    [btnDislike setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [view addSubview:btnDislike];
+    
+    
+    UIButton *btnLike = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnLike.frame = CGRectMake(frame.size.width-w-kPadding, y, w, h);
+    btnLike.backgroundColor = kGreen;
+    btnLike.layer.borderColor = [[UIColor grayColor] CGColor];
+    btnLike.layer.borderWidth = 0.5f;
+    btnLike.layer.cornerRadius = 2.0f;
+    btnLike.layer.masksToBounds = YES;
+    btnLike.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    [btnLike addTarget:self action:@selector(likeArticle) forControlEvents:UIControlEventTouchUpInside];
+    [btnLike setTitle:@"KEEP" forState:UIControlStateNormal];
+    [btnLike setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [view addSubview:btnLike];
     
     
     self.view = view;
@@ -64,7 +104,222 @@
     [self.searchBar resignFirstResponder];
 }
 
+- (void)setCurrentArticle:(PSArticle *)currentArticle
+{
+    _currentArticle = currentArticle;
+    if (currentArticle==nil)
+        return;
+    
+    NSLog(@"CURRENT ARTICLE: %@", currentArticle.title);
+}
 
+- (void)searchArticles:(NSString *)searchTerm
+{
+    [self.loadingIndicator startLoading];
+    NSString *offset = [NSString stringWithFormat:@"%d", self.index];
+    [[PSWebServices sharedInstance] searchArticles:@{@"term":searchTerm, @"offset":offset, @"limit":@"10", @"device":self.device.uniqueId} completionBlock:^(id result, NSError *error){
+        [self.loadingIndicator stopLoading];
+        if (error){
+            [self showAlertWithTitle:@"Error" message:[error localizedDescription]];
+            return;
+        }
+        
+        NSDictionary *response = (NSDictionary *)result;
+        //        NSLog(@"%@", [response description]);
+        NSDictionary *deviceInfo = response[@"device"];
+        if (deviceInfo)
+            [self.device populate:deviceInfo];
+        
+        NSArray *results = response[@"results"];
+        for (int i=0; i<results.count; i++) {
+            PSArticle *article = [PSArticle articleWithInfo:results[i]];
+            [self.searchResults addObject:article];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            int max = (self.searchResults.count >= kSetSize) ? kSetSize : (int)self.searchResults.count;
+            [self animateFeaturedArticles:max];
+            [self findCurrentArticle];
+        });
+    }];
+}
+
+
+
+- (void)animateFeaturedArticles:(int)max
+{
+    CGRect frame = self.view.frame;
+    
+    for (int i=0; i<self.searchResults.count; i++) {
+        int idx = (int)self.searchResults.count-i-1; // adjust index to show articles in correct sequence
+        
+        PSArticle *article = self.searchResults[idx];
+        
+        CGFloat x = (i%2 == 0) ? -frame.size.width : frame.size.width;
+        int index = i%max;
+        PSArticleView *articleView = [PSArticleView articleViewWithFrame:CGRectMake(x, self.searchBar.frame.size.height+kPadding, frame.size.width-2*kPadding, frame.size.height-160.0f)];
+        articleView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        articleView.backgroundColor = [UIColor whiteColor];
+        
+        articleView.tag = 1000 + index;
+        articleView.lblAuthors.text = article.authorsString;
+        articleView.lblTitle.text = article.title;
+        articleView.lblDate.text = article.date;
+        articleView.lblJournal.text = article.journal[@"iso"];
+        articleView.lblAbsratct.text = article.abstract;
+        
+        [self.view addSubview:articleView];
+        self.topView = articleView;
+        [self.loadingIndicator stopLoading];
+        
+        [UIView animateWithDuration:1.65f
+                              delay:(index*0.18f)
+             usingSpringWithDamping:0.5f
+              initialSpringVelocity:0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             CGRect frame = articleView.frame;
+                             frame.origin.x = kPadding;
+                             articleView.frame = frame;
+                         }
+                         completion:^(BOOL finished){
+                             
+                         }];
+    }
+    
+    self.topView.delegate = self;
+}
+
+
+- (void)findCurrentArticle
+{
+    NSLog(@"Find Current Article: %d", (int)self.searchResults.count);
+    if (self.searchResults.count == 0)
+        return;
+    
+    if (self.currentArticle){
+        [self.searchResults removeObject:self.currentArticle];
+        self.currentArticle = nil;
+    }
+    
+    if (self.searchResults.count == 0){
+        NSLog(@"NO MORE ARTICLES!");
+//        [self searchRandomArticles];
+        [self searchArticles:self.searchBar.text];
+        return;
+    }
+    
+    self.currentArticle = self.searchResults[0];
+    self.index++;
+    NSLog(@"CURRENT INDEX: %d", self.index);
+}
+
+- (void)articleViewTapped:(NSInteger)tag
+{
+    NSLog(@"articleViewTapped: %@", self.currentArticle.title);
+    PSArticleViewController *articleVc = [[PSArticleViewController alloc] init];
+    articleVc.article = self.currentArticle;
+    [self.navigationController pushViewController:articleVc animated:YES];
+    
+}
+
+
+- (void)articleViewStoppedMoving
+{
+    CGRect frame = self.topView.frame;
+    CGFloat nuetral = 90.0f;
+    
+    if (frame.origin.x > kPadding+nuetral){
+        [self likeArticle];
+        return;
+    }
+    
+    if (frame.origin.x < kPadding-nuetral){
+        [self dislikeArticle];
+        return;
+    }
+    
+    // neutral
+    [UIView animateWithDuration:0.3f
+                          delay:0
+         usingSpringWithDamping:0.5f
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.topView.frame;
+                         frame.origin.x = kPadding;
+                         frame.origin.y = kPadding+self.searchBar.frame.size.height;
+                         self.topView.frame = frame;
+                         
+                     }
+                     completion:NULL];
+}
+
+
+- (void)queueNextArticle
+{
+    //    NSLog(@"queueNextArticle: %d", (int)self.topView.tag);
+    if (self.topView.tag == 1000){
+        [self.topView removeFromSuperview];
+        self.topView.delegate = nil;
+        self.topView = nil;
+        
+        [self findCurrentArticle];
+        [self animateFeaturedArticles:5]; // load next set of 5
+        return;
+    }
+    
+    int tag = (int)self.topView.tag;
+    [self.topView removeFromSuperview];
+    self.topView = (PSArticleView *)[self.view viewWithTag:tag-1];
+    self.topView.delegate = self;
+    
+    // assign current article
+    [self findCurrentArticle];
+}
+
+
+- (void)dislikeArticle
+{
+    //    NSLog(@"DIS-LIKE Article");
+    [UIView animateWithDuration:0.20f
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         CGRect frame = self.topView.frame;
+                         frame.origin.x = -self.view.frame.size.width-30.0f;
+                         self.topView.frame = frame;
+                         
+                     }
+                     completion:^(BOOL finished){
+                         if (self.searchResults.count > 0){
+                             [self queueNextArticle];
+                         }
+                     }];
+}
+
+
+- (void)likeArticle
+{
+    NSLog(@"LIKE Article: %@", self.currentArticle.title);
+    [self.device saveArticle:self.currentArticle.pmid];
+    
+    [UIView animateWithDuration:0.20f
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         CGRect frame = self.topView.frame;
+                         frame.origin.x = self.view.frame.size.width+30.0f;
+                         self.topView.frame = frame;
+                         
+                     }
+                     completion:^(BOOL finished){
+                         if (self.searchResults.count > 0){
+                             [self queueNextArticle];
+                         }
+                         
+                     }];
+}
 
 
 #pragma mark - SearchBarDelegate
@@ -76,39 +331,14 @@
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
-    NSLog(@"searchBarSearchButtonClicked: %@", searchBar.text);
+    self.index = 0;
     [searchBar resignFirstResponder];
-    
-    [self.loadingIndicator startLoading];
-    [[PSWebServices sharedInstance] searchArticles:@{@"term":searchBar.text, @"limit":@"50", @"device":self.device.uniqueId} completionBlock:^(id result, NSError *error){
-        [self.loadingIndicator stopLoading];
-        if (error){
-            [self showAlertWithTitle:@"Error" message:[error localizedDescription]];
-            return;
-        }
-        
-        NSDictionary *response = (NSDictionary *)result;
-        NSLog(@"%@", [response description]);
-        NSArray *results = response[@"results"];
-        for (int i=0; i<results.count; i++) {
-            PSArticle *article = [PSArticle articleWithInfo:results[i]];
-            [self.searchResults addObject:article];
-        }
-        
-        NSDictionary *deviceInfo = response[@"device"];
-        if (deviceInfo)
-            [self.device populate:deviceInfo];
-
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-        });
-        
-        
-        
-    }];
-    
+    [self searchArticles:searchBar.text];
 }
+
+
+
+
 
 //- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar;                     // called when text starts editing
 //- (BOOL)searchBarShouldEndEditing:(UISearchBar *)searchBar;                        // return NO to not resign first responder
