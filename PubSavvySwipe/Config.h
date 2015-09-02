967e6b352864dd8c071a95aae857c32b25436269
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-09-02 13:27:32 -0400

    adjusted light blue color constant for match background image of cards

--- PubSavvySwipe/Config.h
+++ PubSavvySwipe/Config.h
@@ -19,11 +19,11 @@
 #define kLoggedInNotification       @"loggedInNotification"
 
 
-//#define kAutoSearchId @"55986a4717af620b2d1676d4" // DEV
-#define kAutoSearchId @"5591a7ed6092181100f9fe79" // PRODUCTION
+#define kAutoSearchId @"55986a4717af620b2d1676d4" // DEV
+//#define kAutoSearchId @"5591a7ed6092181100f9fe79" // PRODUCTION
 
 #define kBaseFontName @"Heiti SC"
-#define kLightBlue  [UIColor colorWithRed:62.0f/255.0f green:184.0f/255.0f blue:210.0f/255.0f alpha:1.0f]
+#define kLightBlue  [UIColor colorWithRed:66.0f/255.0f green:196.0f/255.0f blue:221.0f/255.0f alpha:1.0f]
 #define kOrange     [UIColor colorWithRed:215.0f/255.0f green:122.0f/255.0f blue:68.0f/255.0f alpha:1.0f]
 #define kGreen      [UIColor colorWithRed:110.0f/255.0f green:155.0f/255.0f blue:166.0f/255.0f alpha:1.0f]
 #define kBlue       [UIColor colorWithRed:1.0f/255.0f green:147.0f/255.0f blue:183.0f/255.0f alpha:1.0f]
